e2e43970081044c8e40cb8c525b3c7ae58a6db65
Flores <gerardo01302011@gmail.com>
2020-09-17 20:15:07 -0600

    Subiendo metodo POST y GET

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,13 +1,20 @@
 import Product from "../models/Product";
 
 //Funciones que operan con mi BD (Controladores)
-export const createProduct = (req, res) => {
-  console.log(req.body);
-  res.json("Crear producto");
+export const createProduct = async (req, res) => {
+  //1.1 Desestructuro el modelo
+  const { name, category, price, imgURL } = req.body;
+  //1.2 Mando a llamar el modelo de mi Schema paara crear un producto
+  const toCreateProduct = new Product({ name, category, price, imgURL });
+  //1.3 Para que el producto a crear se guarde en la BD (Como tarda en crear un producto es un proceso asincrono)
+  const productSaved = await toCreateProduct.save();
+  //1.4 Creamos un codigo de estado (201 que especifica que un recurso se creo)
+  res.status(201).json(productSaved);
 };
 
-export const getProduct = (req, res) => {
-  res.json("Get products!");
+export const getProduct = async (req, res) => {
+  const allProducts = await Product.find();
+  res.json(allProducts);
 };
 export const getProductById = (req, res) => {
   res.json("Get products por id!");
